2c22b6ef3fb28a737a29fdb6792b73423b42ebc7
DiogoLuisFC <66972937+DiogoLuisFC@users.noreply.github.com>
2022-04-03 23:24:03 -0300

    Update ListApp.java

--- Projeto-1/ListApp.java
+++ Projeto-1/ListApp.java
@@ -2,7 +2,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Random;
+
+// import java.util.Random;
 import figures.*;
 
 class ListApp {
@@ -14,12 +15,16 @@ class ListApp {
 
 class ListFrame extends JFrame {
     ArrayList<Figure> figs = new ArrayList<Figure>();
-    Random rand = new Random();
+    // Random rand = new Random();
     Figure focus = null;
     int x_cursor;
     int y_cursor;
+    int xf;
+    int yf;
+    boolean r = false;
 
     ListFrame () {
+        this.setFocusTraversalKeysEnabled(false);
         this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
@@ -35,10 +40,19 @@ class ListFrame extends JFrame {
                         if (fig.clicked(click.getX(), click.getY())){
                             focus = fig;
                             fig.color= Color.red;
+                            xf =click.getX() - fig.x;
+                            yf =click.getY() - fig.y;
+                            // setCursor(new Cursor(Cursor.MOVE_CURSOR));
                         }else{
                             fig.color= Color.black;
                         }
                     }
+                    if(focus.focusToRize(click.getX(), click.getY())){
+                        r = true;
+                    }else{
+                        r = false;
+                    }
+                    // focus.focusToRize(click.getX(), click.getY())!=0
                     repaint();
                 }
             }
@@ -47,13 +61,29 @@ class ListFrame extends JFrame {
         	new MouseMotionAdapter() {
                 public void mouseMoved(MouseEvent cursor) {
                     x_cursor =cursor.getX();
-                    y_cursor=cursor.getY();              
+                    y_cursor=cursor.getY();
+                    if(focus!= null && focus.clicked(x_cursor,y_cursor)){
+                        
+                        if(focus.focusToRize(x_cursor,y_cursor)==true){
+                            setCursor(new Cursor(Cursor.SE_RESIZE_CURSOR));
+                        }else{
+                            setCursor(new Cursor(Cursor.MOVE_CURSOR));
+                        }  
+
+                    }else{
+                        setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+                    } 
                 }
                 public void mouseDragged(MouseEvent evt) {
-                    if (focus != null) {
-                        focus.drag(evt.getX(),evt.getY());
-        	            repaint();
+                    if (focus != null && r == false) {    
+                        focus.drag(evt.getX(),evt.getY(),xf,yf);    
                     }
+                    else{
+                        focus.resize(evt.getX(),evt.getY());
+                    }
+                    // focus.focusToRize(evt.getX(), evt.getY())==0
+                    // focus.resize(evt.getX(),evt.getY());
+                    repaint();
                 }    
             }
             
@@ -61,10 +91,10 @@ class ListFrame extends JFrame {
         this.addKeyListener (
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
-                    int x = x_cursor;
-                    int y = y_cursor;
-                    int w = rand.nextInt(50);
-                    int h = rand.nextInt(50);
+                    int w = 70;
+                    int h = 60;
+                    int x = x_cursor - w/2;
+                    int y = y_cursor - h/2;
                     if (evt.getKeyChar() == 'r') {
                         Rect r = new Rect(x,y, w,h);
                         figs.add(r);                        
@@ -85,6 +115,31 @@ class ListFrame extends JFrame {
                         figs.remove(focus);
                         focus = null;
                     } 
+                    else if(evt.getKeyCode() == KeyEvent.VK_UP){
+                        focus.y = focus.y - 2;
+                    } 
+                    else if(evt.getKeyCode() == KeyEvent.VK_DOWN){
+                        focus.y = focus.y + 2;
+                    } 
+                    else if(evt.getKeyCode() == KeyEvent.VK_LEFT){
+                        focus.x = focus.x - 2;
+                    } 
+                    else if(evt.getKeyCode() == KeyEvent.VK_RIGHT){
+                        focus.x = focus.x + 2;
+                    }
+                    else if(evt.getKeyCode() == KeyEvent.VK_TAB){
+                        if(focus != null){
+                            int i;
+                            if(figs.indexOf(focus)==figs.size()-1){
+                                i = 0;
+                            }else{
+                                i = figs.indexOf(focus) + 1;
+                            }
+                            focus.color= Color.black;
+                            focus = figs.get(i);
+                            focus.color= Color.red;
+                        }
+                    }
                     repaint();
 
                 }
@@ -92,7 +147,8 @@ class ListFrame extends JFrame {
         );
 
         this.setTitle("Projeto-1");
-        this.setSize(350, 350);
+        this.setSize(500, 500);
+        this.setLocation(450, 100);
     }
     public void paint (Graphics g) {
         super.paint(g);
