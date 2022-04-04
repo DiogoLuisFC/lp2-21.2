b6fc5829e61ce39256f643dc2a74bd4339b9dfce
DiogoLuisFC <66972937+DiogoLuisFC@users.noreply.github.com>
2022-04-03 23:25:39 -0300

    Update Line.java

--- Projeto-1/figures/Line.java
+++ Projeto-1/figures/Line.java
@@ -1,22 +1,32 @@
 package figures;
 
 import java.awt.*;
-// import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+//import java.awt.geom.Ellipse2D.Double;
 
-
-public class Line extends Figure {
-
-    public Line (int x, int y, int w, int h) {
+public class Ellipse extends Figure {
+    public Ellipse (int x, int y, int w, int h) {
         super(x,y, w,h);
     }
+
     public void print () {
-        System.out.format("Linha de tamanho (%d,%d) na posicao (%d,%d).\n",
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.w, this.h, this.x, this.y);
     }
+
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(color);
-        g2d.drawLine(this.x, this.y, this.w + this.x, this.h + this.y);
-
+        // GradientPaint paint = new GradientPaint(200,100,Color.PINK,100,0,Color.BLUE);
+        
+        // g2d.setPaint(paint);
+        g2d.setColor(Color.pink);
+        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.setColor(Color.black);
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+   
+        if(color==Color.red){
+            g2d.setColor(color);
+            g2d.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);
+        }
     }
 }
