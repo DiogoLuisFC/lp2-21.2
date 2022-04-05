30b18f3fe2eb0ae710d6b10370c14b4d5a9acb79
DiogoLuisFC <66972937+DiogoLuisFC@users.noreply.github.com>
2022-04-05 13:44:13 -0300

    Update Line.java

--- Projeto-1/figures/Line.java
+++ Projeto-1/figures/Line.java
@@ -1,8 +1,5 @@
 package figures;
-
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
-//import java.awt.geom.Ellipse2D.Double;
 
 public class Line extends Figure {
     public Line (int x, int y, int w, int h) {
@@ -16,13 +13,9 @@ public class Line extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        // GradientPaint paint = new GradientPaint(200,100,Color.PINK,100,0,Color.BLUE);
-        
-        // g2d.setPaint(paint);
-        g2d.setColor(Color.pink);
-        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+
         g2d.setColor(Color.black);
-        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.drawLine(this.x, this.y, this.w + this.x, this.h + this.y);
    
         if(color==Color.red){
             g2d.setColor(color);
