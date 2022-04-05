6ee51d2d88110aab7d08c3aae1c037f22e7f859b
DiogoLuisFC <66972937+DiogoLuisFC@users.noreply.github.com>
2022-04-05 13:31:28 -0300

    Update Line.java

--- Projeto-1/figures/Line.java
+++ Projeto-1/figures/Line.java
@@ -4,13 +4,13 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 //import java.awt.geom.Ellipse2D.Double;
 
-public class Ellipse extends Figure {
-    public Ellipse (int x, int y, int w, int h) {
+public class Line extends Figure {
+    public Line (int x, int y, int w, int h) {
         super(x,y, w,h);
     }
 
     public void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+        System.out.format("Linha de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.w, this.h, this.x, this.y);
     }
 
