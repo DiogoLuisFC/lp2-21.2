5c08826b1193a060345e2b60b651b41ba7854c43
DiogoLuisFC <66972937+DiogoLuisFC@users.noreply.github.com>
2022-05-18 08:39:09 -0300

    Update Triang.java

--- Projeto-1/figures/Triang.java
+++ Projeto-1/figures/Triang.java
@@ -7,7 +7,7 @@ public class Triang extends Figure {
     public Triang (int x, int y, int w, int h) {
         super(x,y, w,h);
     }    
-    public void paint (Graphics g) {
+    public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
 
         int[] xPoints = {this.x,this.x+this.w,this.x+(this.w/2)};
@@ -20,11 +20,17 @@ public class Triang extends Figure {
         g2d.fillPolygon(xPoints, yPoints, 3);
         g2d.setColor(Color.black);
         g2d.drawPolygon(xPoints, yPoints, 3);
+        
 
         // g2d.fillPolygon(xPoints, yPoints, 3);
-        if(color==Color.red){
-            g2d.setColor(color);
+        // if(color==Color.red){
+        //     g2d.setColor(color);
+        //     g2d.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);
+        // }
+        if(focused){
+            g2d.setColor(Color.red);
             g2d.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);
         }
     }
+
 }
