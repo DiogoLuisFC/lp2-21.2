cdfb0c23f0fe9ca420c4e066c253965839cdbc59
DiogoLuisFC <66972937+DiogoLuisFC@users.noreply.github.com>
2022-05-18 08:38:21 -0300

    Update Line.java

--- Projeto-1/figures/Line.java
+++ Projeto-1/figures/Line.java
@@ -11,7 +11,7 @@ public class Line extends Figure {
             this.w, this.h, this.x, this.y);
     }
 
-    public void paint (Graphics g) {
+    public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
 
         if(colorBG==null){
@@ -20,8 +20,12 @@ public class Line extends Figure {
         g2d.setColor(colorBG);
         g2d.drawLine(this.x, this.y, this.w + this.x, this.h + this.y);
    
-        if(color==Color.red){
-            g2d.setColor(color);
+        // if(color==Color.red){
+        //     g2d.setColor(color);
+        //     g2d.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);
+        // }
+        if(focused){
+            g2d.setColor(Color.red);
             g2d.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);
         }
     }
