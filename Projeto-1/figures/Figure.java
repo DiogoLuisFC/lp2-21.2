61f4c969e258336218918b287f461e5793914624
DiogoLuisFC <66972937+DiogoLuisFC@users.noreply.github.com>
2022-05-23 20:17:38 -0300

    Update Figure.java

--- Projeto-1/figures/Figure.java
+++ Projeto-1/figures/Figure.java
@@ -30,7 +30,7 @@ public abstract class Figure implements IVisible, Serializable  {
         return (this.x <= x && (this.x + this.w) >= x && this.y <= y && (this.y + this.h) >= y );       				
     }
 
-    public boolean focusToRize (int x, int y) {
+    public String focusToRize (int x, int y) {
                 
         // if ( (x == this.x || x == this.x + this.w) && (y >= this.y && y<= (this.y + this.h)) ){
         //     return 1;
@@ -40,13 +40,67 @@ public abstract class Figure implements IVisible, Serializable  {
 		// return 0;	
         // }
     //    return(x == this.x + this.w && y == this.y + this.h);
-    return(x >= this.x + this.w - 5 && x <= this.x + this.w && y >= this.y + this.h - 5 && y <= this.y + this.h);
+    // return(x >= this.x + this.w - 5 && x <= this.x + this.w && y >= this.y + this.h - 5 && y <= this.y + this.h);
+    if(x> this.x+this.w-5 && x< this.x+this.w+5 && y>this.y+5 && y<this.y + this.h - 5){
+        return "r";
+
+    }else if(x>this.x+5 && x<this.x+this.w-5 && y>this.y+this.h-5 && y<this.y+this.h+5){
+        return "b";
+
+    }else if(x>this.x-5 && x<this.x+5 && y>this.y+5 && y<this.y+this.h-5 ){
+        return "l";
+
+    }else if(x>this.x+5 && x<this.x+this.w-5 && y>this.y-5 && y<this.y+5){
+        return "t";
+
+    }else if(x >= this.x + this.w - 5 && x <= this.x + this.w && y >= this.y + this.h - 5 && y <= this.y + this.h){
+        return "rb";
+
+    }else if(x>=this.x && x<=this.x+5 && y>=this.y && y<=this.y+5){
+        return "lt";
+
+    }else if(x >= this.x + this.w - 5 && x <= this.x + this.w && y>=this.y && y<=this.y+5){
+        return "tr";
+
+    }else if(x>=this.x && x<=this.x+5 && y>=this.y+this.h-5 && y<=this.y+this.h){
+        return "bl";
+    }
+    else{
+        return "n";
+    }
 
     }
 
-    public void resize (int dx, int dy){
-        this.w = dx - this.x;
-        this.h = dy - this.y;
+    public void resize (int dx, int dy, String side){
+        // this.w = dx - this.x;
+        // this.h = dy - this.y;
+        if(side=="r"){
+            if(dx>=this.x)this.w = dx-this.x;
+        }else if(side=="b"){
+            if(dy>=this.y)this.h = dy - this.y;
+        }else if(side=="l"){
+            if(dx<=this.x+this.w){
+                this.w= this.w+(this.x-dx);
+                this.x=dx;
+            }
+        }else if(side=="t"){
+            if(dy<=this.y+this.h){
+                this.h= this.h+(this.y-dy);
+                this.y=dy;
+            } 
+        }else if(side=="rb"){
+            this.resize(dx, dy, "r");
+            this.resize(dx, dy, "b");
+        }else if(side=="lt"){
+            this.resize(dx, dy, "l");
+            this.resize(dx, dy, "t");
+        }else if(side=="tr"){
+            this.resize(dx, dy, "t");
+            this.resize(dx, dy, "r");
+        }else if(side=="bl"){
+            this.resize(dx, dy, "b");
+            this.resize(dx, dy, "l");
+        }
     }
     // public void rotate(Graphics g){
     //     Graphics2D g2d = (Graphics2D) g;
